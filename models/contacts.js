dc745b781690c8e62ea7409fa332e2d46de62be9
GuvidSlayer <andgeorge0015@gmail.com>
2024-10-09 17:16:14 +0300

    hw-02 attempt

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,72 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsFilePath = path.resolve("models", "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const readContactsFile = async () => {
+  try {
+    const data = await fs.readFile(contactsFilePath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts.sort((a, b) => a.name.localeCompare(b.name));
+  } catch (error) {
+    return [];
+  }
+};
 
-const removeContact = async (contactId) => {}
+const writeContactsFile = async (contacts) => {
+  try {
+    await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    throw new Error("Error writing to contacts file");
+  }
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const contacts = await readContactsFile();
+  return contacts;
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContactsFile();
+  const contact = contacts.find((c) => c.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await readContactsFile();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
+  const deletedContact = contacts.splice(index, 1)[0];
+  await writeContactsFile(contacts);
+  return deletedContact;
+};
+
+const addContact = async (body) => {
+  const contacts = await readContactsFile();
+  const existingContact = contacts.find((c) => c.email === body.email);
+  if (existingContact) {
+    throw new Error("Contact with this email already exists");
+  }
+  const newContact = { ...body, id: Date.now().toString() };
+  contacts.push(newContact);
+  await writeContactsFile(contacts);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await readContactsFile();
+  const index = contacts.findIndex((c) => c.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const updatedContact = { ...contacts[index], ...body };
+  contacts[index] = updatedContact;
+  await writeContactsFile(contacts);
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +74,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};

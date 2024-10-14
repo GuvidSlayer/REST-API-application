380e978a9fe77259c8051b92ef3d9fb335ec7b15
GuvidSlayer <andgeorge0015@gmail.com>
2024-10-14 14:21:36 +0300

    s

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,123 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const Contact = require("../../service/models/contact.js");
 
-const router = express.Router()
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .required(),
+  favorite: Joi.boolean(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await Contact.findById(contactId);
+    if (!contact) {
+      res.status(404).json({ message: "Contact not found." });
+      return;
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "Missing required name - field." });
+      return;
+    }
+    const newContact = new Contact({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      favorite: req.body.favorite || false,
+    });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    const savedContact = await newContact.save();
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.status(201).json(savedContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const deletedContact = await Contact.findByIdAndDelete(contactId);
+    if (!deletedContact) {
+      res.status(404).json({ message: "Contact not found." });
+      return;
+    }
+    res.status(200).json({ message: "Contact deleted." });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "Missing fields." });
+      return;
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      req.body,
+      { new: true }
+    );
+    if (!updatedContact) {
+      res.status(404).json({ message: "Not found." });
+      return;
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+
+  try {
+    if (favorite === undefined) {
+      res.status(400).json({ message: "missing field favorite" });
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true }
+    );
+
+    if (!updatedContact) {
+      res.status(404).json({ message: "Not found." });
+      return;
+    }
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,51 @@
-const app = require("./app");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const apiRouter = require("./routes/api/contacts.js");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+dotenv.config();
+
+const { DB_HOST: urlDb } = process.env;
+const PORT = process.env.MAIN_PORT;
+
+console.log(urlDb);
+const connection = mongoose.connect(urlDb);
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", apiRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: `Not found - ${req.path}` });
 });
+
+app.use((err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    return res.status(400).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message || "Something went wrong" });
+  }
+});
+
+const startServer = async () => {
+  try {
+    await connection;
+    console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+startServer();
